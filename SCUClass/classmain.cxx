91ce8a4a19be641cf25608d188585c4c1bdad379
Anish Shanmuganathan <55364069+anishshanmug@users.noreply.github.com>
2023-10-10 14:08:55 -0700

    Create classmain.cxx

---
+++ SCUClass/classmain.cxx
@@ -0,0 +1,14 @@
+/*
+ * Name: Anish Shanmuganathan
+ * Email: ashanmuganathan@scu.edu
+ *
+ * Assignment: Container.
+ * 4 of 4: SCU Container main file.
+ */
+
+#include "scuclass.h"
+
+using namespace coen79;
+
+int main() {
+}
